793994f8816ff1a5f663e6f0ef91d82fe8e13914
Mucahit Polat <mupolat@k1m39s01.42istanbul.com.tr>
2023-03-11 10:14:25 +0300

    MINITALK

---
+++ minitalk.h
@@ -0,0 +1,14 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include    <unistd.h>
+#include    <stdlib.h>
+#include    <signal.h>
+
+int     ft_atoi(const char *str);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_strchr(const char *s, int c);
+
+#endif
